0041fd12c952f6f688f39c365dea58a8937ac8f7
msamgan <mohdsamgankhan@gmail.com>
2024-05-24 21:36:46 -0400

    update: add entity link in header.

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -34,6 +34,43 @@ export default function AuthenticatedLayout({ user, header, children, subMenu })
                                             <span className="inline-flex rounded-md">
                                                 <button
                                                     type="button"
+                                                    className="inline-flex items-center px-3 py-2"
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                         viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                                                         className="w-6 h-6 text-white">
+                                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                                              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </Dropdown.Trigger>
+
+                                        <Dropdown.Content>
+                                            <Dropdown.Link className={"text-xl"} href={route("client.create")}>
+                                                Add Client
+                                            </Dropdown.Link>
+                                            <hr />
+                                            <Dropdown.Link className={"text-xl"} href={route("organization.create")}>
+                                                Add Organization
+                                            </Dropdown.Link>
+                                            <hr />
+                                            <Dropdown.Link className={"text-xl"} href={route("project.create")}>
+                                                Add Project
+                                            </Dropdown.Link>
+                                            <hr />
+                                            <Dropdown.Link className={"text-xl"} href={route("transaction.create")}>
+                                                Add Transaction
+                                            </Dropdown.Link>
+                                        </Dropdown.Content>
+                                    </Dropdown>
+                                </div>
+                                <div className="ms-3 relative">
+                                    <Dropdown>
+                                        <Dropdown.Trigger>
+                                            <span className="inline-flex rounded-md">
+                                                <button
+                                                    type="button"
                                                     className="inline-flex items-center px-3 py-2 border border-transparent leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 text-xl"
                                                 >
                                                     {user.name}
