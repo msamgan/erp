f0a99949a672018d5e3e1a27a352fd9993617f71
msamgan <mohdsamgankhan@gmail.com>
2024-05-26 08:38:54 -0400

    added deploy.php

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/msamgan/erp.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('146.190.32.125')
+    ->set('remote_user', 'msamgan-erp')
+    ->set('deploy_path', '~/htdocs/erp.msamgan.com');
+
+task('build', function () {
+    cd('{{release_path}}');
+    run('php artisan optimize');
+    run('npm install');
+    run('npm run build');
+});
+
+after('deploy:update_code', 'build');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
