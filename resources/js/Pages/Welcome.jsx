84dca0f259ebc9574b5983899b4e422e17d8d489
msamgan <mohdsamgankhan@gmail.com>
2024-06-01 07:54:29 -0400

    updated footer.

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -50,7 +50,7 @@ export default function Welcome({}) {
 
             <footer className="bg-gray-800 text-white py-6 mt-20">
                 <div className="container mx-auto text-center">
-                    <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+                    <p>&copy; {new Date().getFullYear()} ERP. All rights reserved.</p>
                 </div>
             </footer>
         </>
