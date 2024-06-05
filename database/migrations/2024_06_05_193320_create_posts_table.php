e79b68d01035a1956c39fd525306ad5e99128ff0
msamgan <mohdsamgankhan@gmail.com>
2024-06-05 15:47:34 -0400

    Posts DB created.

---
+++ database/migrations/2024_06_05_193320_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('content');
+            $table->string('excerpt');
+            $table->string('slug')->unique();
+            $table->timestamp('published_at')->nullable();
+            $table->string('status')->default('draft');
+            $table->string('featured_image')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
