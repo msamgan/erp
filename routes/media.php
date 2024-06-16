168891eacf464c38f9a3f4ffc27e071ecb0f38a5
Mohammed Samgan Khan <mohdsamgankhan@gmail.com>
2024-06-16 14:10:16 -0400

    added the cache to API. (#7)

---
+++ routes/media.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\MediaController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/media', [MediaController::class, 'index'])->name('media');
+    Route::get('/media/unsplash', [MediaController::class, 'unsplash'])->name('media.unsplash');
+    Route::post('/media/store', [MediaController::class, 'store'])->name('media.store');
+    Route::get('/media/photos', [MediaController::class, 'photos'])->name('media.photos');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\MediaController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,16 +16,4 @@ require __DIR__ . '/organization.php';
 require __DIR__ . '/project.php';
 require __DIR__ . '/transaction.php';
 require __DIR__ . '/post.php';
-
-Route::middleware('auth')->group(function () {
-    Route::get('/media', [MediaController::class, 'index'])
-        ->name('media');
-
-    Route::get('/media/unsplash', [MediaController::class, 'unsplash'])
-        ->name('media.unsplash');
-
-    Route::post('/media/store', [MediaController::class, 'store'])->name('media.store');
-
-    Route::get('/media/photos', [MediaController::class, 'photos'])
-        ->name('media.photos');
-});
+require __DIR__ . '/media.php';
