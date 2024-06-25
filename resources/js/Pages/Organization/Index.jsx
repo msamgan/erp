11c022ca7128d6615debe8feace32c3b6e2c2a9e
msamgan <mohdsamgankhan@gmail.com>
2024-06-25 16:18:15 -0400

    Drawer setup.

--- resources/js/Pages/Organization/Index.jsx
+++ resources/js/Pages/Organization/Index.jsx
@@ -6,9 +6,12 @@ import Table from "@/Components/Table.jsx"
 import PrimaryLink from "@/Components/PrimaryLink.jsx"
 import { useEffect, useState } from "react"
 import EditLink from "@/Components/EditLink.jsx"
+import Drawer from "@/Components/Drawer.jsx"
+import PrimaryButton from "@/Components/PrimaryButton.jsx"
 
 export default function Index({ auth, organizations }) {
     const [columns, setColumns] = useState(["Name", "Location", "Actions"])
+    const [openRight, setOpenRight] = useState(false)
     const [data, setData] = useState([])
     const [queryParams, setQueryParams] = useState(
         Object.fromEntries(new URLSearchParams(window.location.search).entries())
@@ -40,11 +43,19 @@ export default function Index({ auth, organizations }) {
             header={<HeaderTitle title="Organizations" />}
             subMenu={
                 <div className="flex space-x-2">
-                    <PrimaryLink
+                    {/*<PrimaryLink
                         className={"h-8"}
                         title="Add Organization"
                         href={route("organization.create")}
-                    />
+                    />*/}
+
+                    <PrimaryButton
+                        className={"h-8"}
+                        title="Add Organization"
+                        onClick={() => setOpenRight(!openRight)}
+                    >
+                        Add Organization
+                    </PrimaryButton>
                 </div>
             }
         >
@@ -57,6 +68,8 @@ export default function Index({ auth, organizations }) {
                     queryParams={queryParams}
                     setQueryParams={setQueryParams}
                 />
+
+                <Drawer open={openRight} side="right" setOpen={setOpenRight}></Drawer>
             </Main>
         </AuthenticatedLayout>
     )
